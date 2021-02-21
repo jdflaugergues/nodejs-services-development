bbc389dd6e9f94c4a56c1ab056add8b959d8eb46
Jonathan de Flaugergues <jonathan.deflaugergues@abbeal.com>
2021-02-21 01:10:42 +0100

    7.3 - combining data

--- consuming-service/routes/root.js
+++ consuming-service/routes/root.js
@@ -2,15 +2,24 @@
 const got = require('got')
 
 const {
-  BICYCLE_SERVICE_PORT = 4000
+  BICYCLE_SERVICE_PORT = 4000,
+  BRAND_SERVICE_PORT = 5000
 } = process.env
 
 const bicycleSrv = `http://localhost:${BICYCLE_SERVICE_PORT}`
+const brandSrv = `http://localhost:${BRAND_SERVICE_PORT}`
 
 module.exports = async function (fastify, opts) {
   fastify.get('/:id', async function (request, reply) {
     const { id } = request.params
-    const bicycle = await got(`${bicycleSrv}/${id}`).json()
-    return bicycle
+    const [ bicycle, brand ] = await Promise.all([
+      got(`${bicycleSrv}/${id}`).json(),
+      got(`${brandSrv}/${id}`).json()
+    ])
+    return {
+      id: bicycle.id,
+      color: bicycle.color,
+      brand: brand.name,
+    }
   })
 }
