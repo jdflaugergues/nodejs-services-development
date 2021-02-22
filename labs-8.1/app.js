1ead34bf1d2774a6a0f05cb7c727a69b883a0bd6
Jonathan de Flaugergues <jonathan.deflaugergues@abbeal.com>
2021-02-23 00:12:02 +0100

    labs 8.1 - implement an HTTP Route-based proxy

---
+++ labs-8.1/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const path = require('path')
+const sensible = require('fastify-sensible')
+const AutoLoad = require('fastify-autoload')
+const replyFrom = require('fastify-reply-from')
+
+
+module.exports = async function (fastify, opts) {
+  fastify.register(sensible)
+  fastify.register(replyFrom)
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+}
