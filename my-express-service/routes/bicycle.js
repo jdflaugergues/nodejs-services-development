2c27335984954d305b13fc9a8f12d0cf6fdf5906
Jonathan de Flaugergues <jonathan.deflaugergues@abbeal.com>
2021-02-24 09:21:14 +0100

    9.2 - route validation with Express

--- my-express-service/routes/bicycle.js
+++ my-express-service/routes/bicycle.js
@@ -2,64 +2,140 @@ const express = require('express')
 const router = express.Router()
 const model = require('../model')
 
-router.get('/:id', (req, res, next) => {
+function hasOwnProperty (o, p) {
+  return Object.prototype.hasOwnProperty.call(o, p)
+}
+
+function validateData (o) {
+  let valid = o !== null && typeof o === 'object'
+  valid = valid && hasOwnProperty(o, 'brand')
+  valid = valid && hasOwnProperty(o, 'color')
+  valid = valid && typeof o.brand === 'string'
+  valid = valid && typeof o.color === 'string'
+  return valid && {
+    brand: o.brand,
+    color: o.color
+  }
+}
+
+function validateBody (o) {
+  let valid = o !== null && typeof o === 'object'
+  valid = valid && hasOwnProperty(o, 'data')
+  valid = valid && o.data !== null && typeof o.data === 'object'
+  const data = valid && validateData(o.data)
+
+  return valid && data && {data}
+}
+
+function isIdValid (n) {
+  n = Number(n)
+  const MAX_SAFE = Math.pow(2, 53) - 1
+
+  return isFinite(n) && Math.floor(n) === n && Math.abs(n) <= MAX_SAFE
+}
+
+function isParamsValid (o) {
+  var valid = o !== null && typeof o === 'object'
+  valid = valid && hasOwnProperty(o, 'id')
+  valid = valid && isIdValid(o.id)
+  return valid
+}
+
+function badRequest () {
+  const err = new Error('Bad Request')
+  err.status = 400
+  return err
+}
+
+router.get('/:id', function (req, res, next) {
+  if (!isParamsValid(req.params)) {
+    return next(badRequest())
+  }
+
   model.bicycle.read(req.params.id, (err, result) => {
     if (err) {
-      if (err.message === 'not found') next()
-      else next(err)
-    } else {
-      res.send(result)
+      if (err.message === 'not found') return next()
+      else return next(err)
     }
+    const sanitizedResult = validateData(result)
+    if (!sanitizedResult) {
+      return next(new Error('Server Error'))
+    }
+    res.send(sanitizedResult)
   })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', function (req, res, next) {
   const id = model.bicycle.uid()
-  model.bicycle.create(id, req.body.data, (err) => {
-    if (err) next(err)
-    else res.status(201).send({ id })
+  const body = validateBody(req.body)
+
+  if (!body) {
+    return next(badRequest())
+  }
+
+  model.bicycle.create(id, body.data, (err) => {
+    if (err) return next(err)
+
+    if (isIdValid(id)) res.status(201).send({ id })
+    else next(new Error('Server Error'))
   })
 })
 
-router.post('/:id/update', (req, res, next) => {
-  model.bicycle.update(req.params.id, req.body.data, (err) => {
+router.post('/:id/update', function (req, res, next) {
+  if (!isParamsValid(req.params)) {
+    return next(badRequest())
+  }
+  const body = validateBody(req.body)
+
+  if (!body) {
+    return next(badRequest())
+  }
+
+  model.bicycle.update(req.params.id, body.data, (err) => {
     if (err) {
-      if (err.message === 'not found') next()
-      else next(err)
-    } else {
-      res.status(204).send()
+      if (err.message === 'not found') return next()
+      else return next(err)
     }
+    res.status(204).send()
   })
 })
 
-router.put('/:id', (req, res, next) => {
-  model.bicycle.create(req.params.id, req.body.data, (err) => {
+router.put('/:id', function (req, res, next) {
+  if (!isParamsValid(req.params)) {
+    return next(badRequest())
+  }
+
+  const body = validateBody(body)
+  if (!body) {
+    return next(badRequest())
+  }
+
+  model.bicycle.create(req.params.id, body.data, (err) => {
     if (err) {
-      if (err.message === 'resource exists') {
-        model.bicycle.update(req.params.id, req.body.data, (err) => {
-          if (err) next(err)
-          else res.status(204).send()
-        })
-      } else {
-        next(err)
+      if (err.message !== 'resource exists') {
+        return next(err)
       }
-    } else {
-      res.status(201).send({})
+      return model.bicycle.update(req.params.id, body.data, (err) => {
+        if (err) return next(err)
+        res.status(204).send()
+      })
     }
+    return res.status(201).send({})
   })
 })
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', function (req, res, next) {
+  if (!isParamsValid(req.params)) {
+    return next(badRequest())
+  }
+
   model.bicycle.del(req.params.id, (err) => {
     if (err) {
-      if (err.message === 'not found') next()
-      else next(err)
-    } else {
-      res.status(204).send()
+      if (err.message === 'not found') return next()
+      else return next(err)
     }
+    res.status(204).send()
   })
 })
-
-
 
 module.exports = router
